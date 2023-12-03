75fcf752d098a8bf35a163c69754b7f23c68734a
Logan Karstens <123521971+lkfau@users.noreply.github.com>
2023-12-02 22:58:19 -0500

    Add files via upload

---
+++ query.sql
@@ -0,0 +1,4 @@
+SELECT lot_id, name
+FROM parking_lot
+order by lot_id
+limit 10
---
+++ test.py
@@ -0,0 +1,44 @@
+import psycopg2
+from configparser import ConfigParser
+
+
+
+def config(filename='database.ini', section='postgresql'):
+    parser = ConfigParser()
+    parser.read(filename)
+
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section ' + section + 'not found.')
+    return db
+
+
+
+def connect():
+    conn = None
+    try:
+        params = config()
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        
+        cur.execute(open("query.sql", "r").read())
+        test = cur.fetchall()
+        for row in test:
+            print(row)
+    
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+
+if __name__ == '__main__':
+    connect()
