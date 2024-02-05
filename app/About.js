60377cb0f02f1d7632cc15418029802fc2e8d3fd
RomeoFrancois <romeofrancois348@gmail.com>
2024-02-05 00:15:11 -0500

    added about page

--- app/About.js
+++ app/About.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+
+const styles = StyleSheet.create({
+  paragraph: {
+    paddingLeft: 50,
+  },
+  title: {
+    textAlign: 'center',
+    paddingTop: 50,
+    fontSize: 25,
+  },
+  container: {
+    paddingTop: 25,
+  },
+  stretch: {
+    width: 100,
+    height: 100,
+    resizeMode: 'stretch',
+  },
+});
+
 
 const About = () => {
   const navigation = useNavigation();
 
   return (
     <View>
-      <Text>About Page</Text>      
+      <Text style={styles.title}>About Page</Text>
+      <Text style = {styles.container}>Mission statement. Why we created this app, what we hope to accomplish, and possible improvements</Text>
+      <View style ={styles.container}>   
+      <Text style = {styles.paragraph}><Image 
+      style={styles.stretch}
+      source={require('./assets/userPlaceholder.png')} />About you and contact information</Text>
+      <Text style = {styles.paragraph}><Image 
+      style={styles.stretch}
+      source={require('./assets/userPlaceholder.png')} />About you and contact information</Text>
+      <Text style = {styles.paragraph}><Image 
+      style={styles.stretch}
+      source={require('./assets/userPlaceholder.png')} />About you and contact information</Text>
+      <Text style = {styles.paragraph}><Image 
+      style={styles.stretch}
+      source={require('./assets/userPlaceholder.png')} />About you and contact information</Text>
+      <Text style = {styles.paragraph}><Image 
+      style={styles.stretch}
+      source={require('./assets/userPlaceholder.png')} />About you and contact information</Text>
+      </View>
     </View>
   );
 }
