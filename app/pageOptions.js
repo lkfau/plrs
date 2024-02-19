fb47cb89baf4e53c3d2bf72f0b8c58d4956d6b7e
lkfau <123521971+lkfau@users.noreply.github.com>
2024-02-18 19:24:30 -0500

    replace stack navigator with tabs

---
+++ app/pageOptions.js
@@ -0,0 +1,17 @@
+import CreateAccountPage from './CreateAccountPage';
+import LoginPage from './LoginPage';
+import HomePage from './HomePage';
+import Account from './MyAccount';
+import Schedules from './SavedSchedules';
+import Recommendation from './Recommendation';
+import About from './About';
+
+export default pageOptions = [
+  { title: 'Home', icon: 'home', component: HomePage, showLoggedIn: 1, showNotLoggedIn: 1 },
+  { title: 'Create Account', icon: 'create', component: CreateAccountPage, showLoggedIn: 1, showLoggedOut: 1 },
+  { title: 'Log In', icon: 'arrow-forward-circle-outline', component: LoginPage,showLoggedIn: 1, showLoggedOut: 1 },
+  { title: 'Account', icon: 'person-circle', component: Account, showLoggedIn: 1, showLoggedOut: 1 },
+  { title: 'Schedules', icon: 'list', component: Schedules, showLoggedIn: 1, showLoggedOut: 1 },
+  { title: 'Recommendation', icon: 'sparkles', component: Recommendation, showLoggedIn: 1, showLoggedOut: 1 },
+  { title: 'About', icon: 'information-circle', component: About, showLoggedIn: 1, showLoggedOut: 1 },
+];
