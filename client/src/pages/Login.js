d2151b76c2bfaf02ad0f1e091530053e52bbdde2
lkfau <123521971+lkfau@users.noreply.github.com>
2024-02-24 20:54:33 -0500

    Login merge

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,6 +2,7 @@ import { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'
 import Button from '../components/UI/Button/Button';
 import AuthContext from '../context/auth-context';
+import styles from './Login.module.css';
 
 const Login = () => {
     const navigate = useNavigate();
@@ -9,7 +10,12 @@ const Login = () => {
 
     useEffect(() => { if (ctx.user_id != null) navigate('/recommend'); }, [ctx, navigate]);
 
-    return <Button onClick={() => ctx.logIn(1)}>Log in as test user</Button>;
+    return (
+        <div className={styles.container}>
+            <h1>Parking Lot Recommendation System</h1>
+            <Button onClick={() => ctx.logIn(1)}>Log in as test user</Button>
+        </div>
+    );
 }
 
 export default Login;
