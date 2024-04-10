8b9bc149fbf3740b1d29762d53baf2efebee77f4
owennlevinee <olevine2020@fau.edu>
2024-04-09 20:05:25 -0400

    added auth to change email and pwd

--- server/src/endpoints/user.py
+++ server/src/endpoints/user.py
@@ -1,7 +1,8 @@
 from flask import request,Blueprint, jsonify
 from flask_cors import cross_origin
 from ..database.db_connection import run_query as query
-from ..security.crypt_functions import session_ids, custom_hash
+from ..security.crypt_functions import custom_hash
+from ..endpoints.login import check_session
 
 
 
@@ -9,6 +10,7 @@ app_user = Blueprint('app_user', __name__)
 @app_user.route('/create_user', methods=['POST'])
 @cross_origin()
 def create_user():
+    
     request_data = request.get_json()
     email = request_data['email']
     pwd = request_data['pwd']
@@ -23,9 +25,16 @@ app_update_user_email = Blueprint('app_update_user_email', __name__)
 @app_update_user_email.route('/update_user_email', methods=['PUT'])
 @cross_origin()
 def update_user_email():
+    bearer = request.headers.get('Authorization')
+
+    if bearer:
+        userinfo = check_session(bearer.split()[1])
+    if (not bearer or not userinfo):
+        return jsonify({'message': 'Unauthorized'}), 401
+    
     request_data = request.get_json()
     email = request_data['email']
-    user_id = request_data['user_id']
+    user_id = userinfo.user_id
     if email == '':
         return 'Error; User must have an email', 404
     user = [email, user_id]
@@ -36,9 +45,16 @@ app_update_user_password = Blueprint('app_update_user_password', __name__)
 @app_update_user_password.route('/update_user_password', methods=['PUT'])
 @cross_origin()
 def update_user_password():
+    bearer = request.headers.get('Authorization')
+
+    if bearer:
+        userinfo = check_session(bearer.split()[1])
+    if (not bearer or not userinfo):
+        return jsonify({'message': 'Unauthorized'}), 401
+    
     request_data = request.get_json()
     pwd = request_data['pwd']
-    user_id = request_data['user_id']
+    user_id = userinfo.user_id
     if pwd == '':
         return jsonify({'message': 'Error; User must have a password'}), 404
     pwd = custom_hash(pwd)
